59a40001575d3f02ebcf5bcfb97803970228476a
Todd Parsons <todd.e.parsons@googlemail.com>
2023-07-03 19:22:18 +0100

    Beginnings of Catpuccin theme

--- app/theme/app/catppuccin.py
+++ app/theme/app/catppuccin.py
@@ -11,6 +11,9 @@ QPalette.Window, QPalette.ToolTipBase,
 QPalette.Shadow, QPalette.Highlight,
 QPalette.Link, QPalette.LinkVisited
 
+### Colour states
+QPalette.Active, QPalette.Disabled, QPalette.Inactive
+
 ### Flavour tags
 rosewater, flamingo, pink, mauve, red, maroon, peach, yellow, green, teal, sky, sapphire, blue, lavender
 text
@@ -19,18 +22,47 @@ overlay2, overlay1, overlay0,
 surface2, surface1, surface0,
 base, mantle, crust,
 """
-def QC(value):
+def set_colors(palette, values):
     """
-    Shorthand for converting a Catppuccin colour to a PyQt colour.
+    Set several colour roles to matching flavour objects
     """
-    return QColor("#" + value.hex)
+    for role, col in values.items():
+        if isinstance(col, dict):
+            for state, col in col.items():
+                palette.setColor(state, role, QColor("#" + col.hex))
+        else:
+            palette.setColor(role, QColor("#" + col.hex))
+
 
 # --- Catppuccin Frappe ---
 _flav = Flavour.frappe()
 frappe = QPalette()
-frappe.setColor(QPalette.Window, QC(_flav.mantle))
-frappe.setColor(QPalette.Button, QC(_flav.crust))
-frappe.setColor(QPalette.Base, QC(_flav.base))
+set_colors(frappe, {
+    QPalette.Button: {
+        QPalette.Active: _flav.crust, 
+        QPalette.Inactive: _flav.blue,
+        QPalette.Disabled: _flav.base,
+    },
+    # QPalette.Light: _flav, 
+    # QPalette.Midlight: _flav, 
+    # QPalette.Mid: _flav,
+    # QPalette.Dark: _flav,
+    # QPalette.WindowText: _flav, 
+    QPalette.Text: _flav.text, 
+    # QPalette.BrightText: _flav, 
+    # QPalette.ButtonText: _flav, 
+    # QPalette.HighlightedText: _flav, 
+    # QPalette.ToolTipText: _flav,
+    # QPalette.PlaceholderText: _flav,
+    QPalette.Base: _flav.base, 
+    # QPalette.AlternateBase: _flav,
+    QPalette.Window: _flav.mantle, 
+    # QPalette.ToolTipBase: _flav,
+    # QPalette.Shadow: _flav, 
+    # QPalette.Highlight: _flav,
+    # QPalette.Link: _flav, 
+    # QPalette.LinkVisited: _flav, 
+})
 
 
 # --- Catppuccin Latte ---
